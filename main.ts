e0810950591867189a383ffadf4fd7a04975208b
Enem <enembarjo03@gmail.com>
2024-08-14 17:21:47 +0530

    open file after creating

--- main.ts
+++ main.ts
@@ -4,7 +4,10 @@ export default class New_File extends Plugin{
 	async onload(){
 		this.addRibbonIcon('file-key','New File',async() =>{
 			const New_File = 'New File.md';
-			await this.app.vault.create(New_File,"");
+			const NewFile = await this.app.vault.create(New_File,"");
+
+			const leaf = this.app.workspace.getLeaf();
+			await leaf.openFile(NewFile);
 		});
 	}
 }
