10cb2dd3f501f10996bd6a91486b81eab6033b2f
Enem <enembarjo03@gmail.com>
2024-09-26 14:53:25 +0530

    create file with date as filename

--- main.ts
+++ main.ts
@@ -1,13 +1,27 @@
-import {Plugin} from 'obsidian';
+import { Plugin } from 'obsidian';
 
-export default class New_File extends Plugin{
-	async onload(){
-		this.addRibbonIcon('file-key','New File',async() =>{
-			const New_File = 'New File.md';
-			const NewFile = await this.app.vault.create(New_File,"");
+export default class NewFilePlugin extends Plugin {
+	async onload() {
+		const date = new Date();
+		// Ribbon icon to create a new file with a default name
+		this.addRibbonIcon('file-key', 'New File', async () => {
+			const newFileName = 'New File.md';
+			const newFile = await this.app.vault.create(newFileName, "");
 
 			const leaf = this.app.workspace.getLeaf();
-			await leaf.openFile(NewFile);
+			await leaf.openFile(newFile);
+		});
+
+		// Ribbon icon to create a new file with the current date and time
+		this.addRibbonIcon('calendar-plus', 'New Dated File', async () => {
+			const date = new Date();
+			const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+			const formattedTime = `${String(date.getHours()).padStart(2, '0')}-${String(date.getMinutes()).padStart(2, '0')}`;
+			const newDatedFileName = `Note_${formattedDate}_${formattedTime}.md`;
+			const newDatedFile = await this.app.vault.create(newDatedFileName, "");
+
+			const leaf = this.app.workspace.getLeaf();
+			await leaf.openFile(newDatedFile);
 		});
 	}
 }
